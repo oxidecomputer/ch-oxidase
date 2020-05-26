55ff2f4a9dd30f4418520bd00d7139912e9043b2
Allison Randal <allison@oxide.computer>
2020-05-26 15:00:09 -0400

    Start adding workspace dependencies

---
+++ arch_gen/src/x86/mod.rs
@@ -0,0 +1,25 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE-BSD-3-Clause file.
+
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(
+    clippy::unreadable_literal,
+    clippy::redundant_static_lifetimes,
+    clippy::trivially_copy_pass_by_ref,
+    clippy::useless_transmute,
+    clippy::should_implement_trait,
+    clippy::transmute_ptr_to_ptr
+)]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+#[allow(clippy::unreadable_literal, clippy::redundant_static_lifetimes)]
+pub mod mpspec;
+#[allow(non_upper_case_globals)]
+#[allow(clippy::unreadable_literal, clippy::redundant_static_lifetimes)]
+pub mod msr_index;
